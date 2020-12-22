308ccf79e2a994a83a351793871da9d3c438b514
dzinoviev <danzino21@gmail.com>
2020-12-22 22:45:06 +0200

    Refactor Prototype name

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 // import App from './patterns/FactoryMethod';
 // import App from './patterns/AbstractFactory';
 // import App from './patterns/Builder';
-import App from './patterns/Clone';
+import App from './patterns/Prototype';
 
 App();
